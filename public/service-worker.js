20b05e0f0803d15911c48ceefedd7de1777f956b
Jack Ceballos <jackrceballos@gmail.com>
2021-03-08 22:12:10 -0500

    Finished the install event

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,30 @@
-/*const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-// FILES_TO_CACHE in the solved of caching files
+
 const CACHE_FILES = [
     "/",
     "/index.js",
     "/index.html",
     "/styles.css",
-    ""
-];*/
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
 
 // Install Event
 self.addEventListener("install", (event) => {
     console.log("Service Worker Installed!");
-})
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            console.log("Service Working Caching Files");
+            cache.addAll(CACHE_FILES);
+        })
+        .then(() => {
+            self.skipWaiting()
+        })
+    )
+});
 
 // Activate Event
 self.addEventListener("activate", (event) => {
