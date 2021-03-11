750dd53cebcc080be240c1a427d93cb4d920fdd1
Jack Ceballos <jackrceballos@gmail.com>
2021-03-10 20:40:39 -0500

    Finshing up the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,25 @@ const config = {
     plugins: [
         new WebpackPwaManifest({
             name: "Finance Tracker",
-            
+            short_name: "Finance Trckr",
+            description: "Progressive Web App that helps track finances.",
+            background_color: "#ffffff",
+            start_url: "/",
+            icons: [
+                {
+                    src: path.resolve("public/icons/icon-192x192.png"),
+                    size: "192x192"
+                },
+                {
+                    src: path.resolve("public/icons/icon-512x512.png"),
+                    size: "512x512"
+                }
+            ],
+            inject: false,
+            fingerprints: false
         })
-    ]
+    ],
+    mode: "development"
 }
+
+module.exports = config;
